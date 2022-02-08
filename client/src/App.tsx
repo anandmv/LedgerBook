b2a4777eb87946bbe7a3ce6603ec813810558b1f
Anand Moothedath Velluva <anandmv@Anands-MacBook-Air.local>
2022-02-08 17:11:43 +0100

    client theme and template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Login from './pages/login';
+import Dashboard from './pages/dashboard'
 
-function App() {
+const Footer = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Welcome
-        </p>
-      </header>
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center">
+      Developed By
+      <Link color="inherit" href="email://anand.mv2@gmail.com">
+        Anand Moothedath Velluva
+      </Link>
+    </Typography>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+      <Footer/>
+    </div>
+  );
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
